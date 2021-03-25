7054ab5460e374ce71dcbce1bce53f50ea9277f1
Lucas Sarkadi <63003495+Slyp05@users.noreply.github.com>
2021-03-25 16:52:20 +0100

    Update ReadOnlyAttribute.cs

--- Inspector Attributes Collection/ReadOnlyAttribute.cs
+++ Inspector Attributes Collection/ReadOnlyAttribute.cs
@@ -4,6 +4,9 @@
  * USAGE:
  *  This attribute will show a field as readonly in the inspector.
  *  
+ * SOURCE:
+ *  https://github.com/Slyp05/Unity-Inspector-Attributes-Collection
+ *
  * */
 namespace InspectorAttribute
 {
